fcacc29681c7d57b3e76a4f2723560d90b09cc4b
Gian Jaskulski <gian@gian.com.br>
2018-05-23 11:56:43 +0200

    aa

--- mycookbook/recipes/default.rb
+++ mycookbook/recipes/default.rb
@@ -2,10 +2,8 @@ hostname = 'ahahahahh'
 file '/etc/hostname' do
     content "#{hostname}\n"
     mode '0644'
-    notifies :reload, 'ohai[reload_hostname]', :immediately
 end
 
 execute "hostname #{hostname}" do
     only_if { node['hostname'] != hostname }
-    notifies :reload, 'ohai[reload_hostname]', :immediately
 end
