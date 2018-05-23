ded3d494a52a3fb08011fad2c76e9d85a3b5634c
Gian Jaskulski <gian@gian.com.br>
2018-05-23 12:04:06 +0200

    aaa

--- mycookbook/recipes/default.rb
+++ mycookbook/recipes/default.rb
@@ -1,7 +1,11 @@
-hostname = 'ahahahahh'
+hostname = node["opsworks"]["instance"]["hostname"]
 file '/etc/hostname' do
     content "#{hostname}\n"
     mode '0644'
+end
+
+execute "sed -i /etc/sysconfig/network -e 's/^HOSTNAME=.*/HOSTNAME=#{hostname}/'" do
+    only_if { node['hostname'] != hostname }
 end
 
 execute "hostname #{hostname}" do
