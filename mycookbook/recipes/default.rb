f50225fd91ae466e9e3abac72ec8f24ae0ebbfe8
Gian Jaskulski <gian@gian.com.br>
2018-05-24 10:23:15 +0200

    aa

--- mycookbook/recipes/default.rb
+++ mycookbook/recipes/default.rb
@@ -1,43 +1,14 @@
 instance = search("aws_opsworks_instance", "self:true").first
 hostname="#{instance['hostname']}"
-id="#{instance['instance_id']}"
 
-
-
-#file '/etc/hostname' do
-#    content "#{hostname}\n"
-#    mode '0644'
-#end
-
-#execute "sed -i /etc/sysconfig/network -e 's/^HOSTNAME=.*/HOSTNAME=#{hostname}/'" do
-#    only_if { node['hostname'] != hostname }
-#end
-
-#execute "hostname #{hostname}" do
-#    only_if { node['hostname'] != hostname }
-#end
-
-def get_opsworks_hostname(instance_id)
-  Chef::Log.info("********** Ran function **********")
-  # Ensure aws-sdk is installed and create OpsWorks Client
-  chef_gem "aws-sdk" do
-    action :install
-    compile_time true
-  end
-  require 'aws-sdk'
-  Aws.use_bundled_cert!
-  client = Aws::OpsWorks::Client.new(
-    region: search("aws_opsworks_stack").first['region']
-  )
-
-  # Get instance Hostname
-  instance_resp = client.describe_instances({
-    instance_ids: [instance_id]
-  })
-  return instance_resp.instances[0].hostname
+execute "sed -i /etc/sysconfig/network -e 's/^HOSTNAME=.*/HOSTNAME=#{hostname}/'" do
+    only_if { node['hostname'] != hostname }
 end
 
-Chef::Log.info("********** Instance Hostname from API: '#{get_opsworks_hostname(id)}' **********")
-Chef::Log.info("********** Instance id: '#{id}' **********")
+execute "hostname #{hostname}" do
+    only_if { node['hostname'] != hostname }
+end
+
+
 Chef::Log.info("********** Instance hostname: '#{hostname}' **********")
 
